3507ba25aa05875b577d73b9ffd642c726baa941
Zzzen <callmetsing@gmail.com>
2022-07-30 17:41:29 +0800

    show symbol of nodes

--- src/compiler/api.ts
+++ src/compiler/api.ts
@@ -13,9 +13,16 @@ export {
 export { ImportResolver } from "@zzzen/pyright-internal/dist/analyzer/importResolver";
 export { Program } from "@zzzen/pyright-internal/dist/analyzer/program";
 export { ParseTreeWalker } from "@zzzen/pyright-internal/dist/analyzer/parseTreeWalker";
-export { ParseNode, ParseNodeType, isExpressionNode } from "@zzzen/pyright-internal/dist/parser/parseNodes";
+export {
+  ParseNode,
+  ParseNodeType,
+  isExpressionNode,
+} from "@zzzen/pyright-internal/dist/parser/parseNodes";
 
-export { findNodeByOffset, printParseNodeType } from "@zzzen/pyright-internal/dist/analyzer/parseTreeUtils";
+export {
+  findNodeByOffset,
+  printParseNodeType,
+} from "@zzzen/pyright-internal/dist/analyzer/parseTreeUtils";
 
 export enum TypeCategory {
   // Name is not bound to a value of any type.
@@ -75,4 +82,58 @@ export enum TypeFlags {
   SpecialForm = 1 << 3,
 }
 
-export const typesheds = import.meta.glob('/node_modules/pyright/dist/typeshed-fallback/stdlib/*.pyi', { as: 'raw' });
+export enum SymbolFlags {
+  None = 0,
+
+  // Indicates that the symbol is unbound at the start of
+  // execution. Some symbols are initialized by the module
+  // loader, so they are bound even before the first statement
+  // in the module is executed.
+  InitiallyUnbound = 1 << 0,
+
+  // Indicates that the symbol is not visible from other files.
+  // Used for module-level symbols.
+  ExternallyHidden = 1 << 1,
+
+  // Indicates that the symbol is a class member of a class.
+  ClassMember = 1 << 2,
+
+  // Indicates that the symbol is an instance member of a class.
+  InstanceMember = 1 << 3,
+
+  // Indicates that the symbol is considered "private" to the
+  // class or module and should not be accessed outside or overridden.
+  PrivateMember = 1 << 5,
+
+  // Indicates that the symbol is not considered for protocol
+  // matching. This applies to some built-in symbols like __class__.
+  IgnoredForProtocolMatch = 1 << 6,
+
+  // Indicates that the symbol is a ClassVar, so it cannot be
+  // set when accessed through a class instance.
+  ClassVar = 1 << 7,
+
+  // Indicates that the symbol is in __all__.
+  InDunderAll = 1 << 8,
+
+  // Indicates that the symbol is a private import in a py.typed module.
+  PrivatePyTypedImport = 1 << 9,
+
+  // Indicates that the symbol is an InitVar as specified in PEP 557.
+  InitVar = 1 << 10,
+}
+
+export enum DeclarationType {
+  Intrinsic,
+  Variable,
+  Parameter,
+  Function,
+  Class,
+  SpecialBuiltInClass,
+  Alias,
+}
+
+export const typesheds = import.meta.glob(
+  "/node_modules/pyright/dist/typeshed-fallback/stdlib/*.pyi",
+  { as: "raw" }
+);
--- src/components/PropertiesViewer.tsx
+++ src/components/PropertiesViewer.tsx
@@ -5,28 +5,30 @@ import {
   printParseNodeType,
   TypeCategory,
   TypeFlags,
+  SymbolFlags,
+  DeclarationType,
 } from "../compiler/api";
 import { AppState } from "../state";
 
 const theme = {
-  scheme: 'monokai',
-  author: 'wimer hazenberg (http://www.monokai.nl)',
-  base00: '#272822',
-  base01: '#383830',
-  base02: '#49483e',
-  base03: '#75715e',
-  base04: '#a59f85',
-  base05: '#f8f8f2',
-  base06: '#f5f4f1',
-  base07: '#f9f8f5',
-  base08: '#f92672',
-  base09: '#fd971f',
-  base0A: '#f4bf75',
-  base0B: '#a6e22e',
-  base0C: '#a1efe4',
-  base0D: '#66d9ef',
-  base0E: '#ae81ff',
-  base0F: '#cc6633',
+  scheme: "monokai",
+  author: "wimer hazenberg (http://www.monokai.nl)",
+  base00: "#272822",
+  base01: "#383830",
+  base02: "#49483e",
+  base03: "#75715e",
+  base04: "#a59f85",
+  base05: "#f8f8f2",
+  base06: "#f5f4f1",
+  base07: "#f9f8f5",
+  base08: "#f92672",
+  base09: "#fd971f",
+  base0A: "#f4bf75",
+  base0B: "#a6e22e",
+  base0C: "#a1efe4",
+  base0D: "#66d9ef",
+  base0E: "#ae81ff",
+  base0F: "#cc6633",
 };
 
 export default function PropertiesViewer() {
@@ -35,7 +37,14 @@ export default function PropertiesViewer() {
   const isExpr = isExpressionNode(selectedNode);
   const type =
     isExpr && state.program.evaluator?.getTypeOfExpression(selectedNode);
-  const decl = (selectedNode.nodeType) === ParseNodeType.Name && state.program.evaluator?.getDeclarationsForNameNode(selectedNode) || "[None]";
+  const decl =
+    (selectedNode.nodeType === ParseNodeType.Name &&
+      state.program.evaluator?.lookUpSymbolRecursive(
+        selectedNode,
+        selectedNode.value,
+        false
+      )?.symbol) ||
+    "[None]";
 
   return (
     <div className="verticalContainer">
@@ -92,19 +101,46 @@ export default function PropertiesViewer() {
           return false;
         }}
       />
-      <h2>Declarations</h2>
+      <h2>Symbol</h2>
       <JSONTree
         theme={theme}
         invertTheme
         data={decl}
         hideRoot
         valueRenderer={(str, val, ...keypaths) => {
+          console.log(str, val, keypaths);
+          if (keypaths[0] === "type" && typeof keypaths[1] === "number" && keypaths[2] === '_declarations') {
+            return val + '(' + DeclarationType[val] + ')';
+          }
           if (keypaths[0] === "nodeType") {
             return printParseNodeType(val);
+          }
+          if (keypaths[0] === "_flags") {
+            return printFlags(val, SymbolFlags);
           }
           return str;
         }}
       />
     </div>
   );
+}
+
+
+function printFlags(flags: number, def: Record<number, string>) {
+  if (flags === 0) {
+    return "None";
+  }
+  const bits = flags.toString(2) as string;
+  const str = bits
+    .split("")
+    .reverse()
+    .map((bit, i) => {
+      if (bit === "0") {
+        return "";
+      }
+      return def[1 << i];
+    })
+    .filter(Boolean)
+    .join("|");
+  return flags + "(" + str + ")";
 }
